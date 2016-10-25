6c84d84448e14b5784c7ab9a33fa4915e2c3a2fc
lassemunk <lassemunkmail@gmail.com>
2016-10-25 22:28:34 +0200

    JAAAA!!!!! Max --> Phone

--- public/js/client.js
+++ public/js/client.js
@@ -1,10 +1,9 @@
-var socket = io();
-socket.connect('http://' + ipAddress + ':' + port);
 
 
+var socket = io().connect();
 socket.on('message', oscMessage); 	// if there is a message called 'message', call function
 													// oscMessage
 function oscMessage(data) {	  		// the content of the 'oscMessage' message, is logged	
-//  ampEnv();
+	ampEnv();
 	console.log("ampenv");		 
 }
